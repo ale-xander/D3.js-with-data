e4224d37f0f2de722564533238c508cd330e3710
alexiliev <iliev.alex@outlook.com>
2021-02-05 11:47:42 -0600

    creating html elements dynamically

--- app.js
+++ app.js
@@ -5,15 +5,36 @@ const data = [
     {
         width: 500,
         height: 200,
-        fill: '#b342e9'
-    }
+        fill: '#b3f2e9'
+    },
+    {
+        width: 100,
+        height: 100,
+        fill: '#6ba9e0'
+    },
+    {
+        width: 1000,
+        height: 10,
+        fill: '#7f74d2'
+    },
 ]
 
 const svg = d3.select('svg')
-const rect = svg.select('rect')
+// (d, i, n) ... i= index of array, n= array of elements
+
+// join data to rect
+const rect = svg.selectAll('rect')
                 .data(data)
-                .attr('width', (d)=> d.width)
-                .attr('height', (d)=> d.height)
-                .attr('fill', (d)=> d.fill)
+                
+// add attrs to rect already in the DOM
+rect.attr('width', (d, i, n)=> d.width)
+    .attr('height', (d)=> d.height)
+    .attr('fill', d => d.fill)
 
 console.log(rect)
+// append the enter selection to the DOM
+rect.enter()
+    .append('rect')
+    .attr('width', (d, i, n)=> d.width)
+    .attr('height', (d)=> d.height)
+    .attr('fill', d => d.fill)
